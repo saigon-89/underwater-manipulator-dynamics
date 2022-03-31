6078738a5fc45798841e2c058cfb5c3eef813d89
saigon-89 <80712391+saigon-89@users.noreply.github.com>
2022-03-31 10:17:45 +0300

    Update RRR_robot.m

--- examples/RRR_robot.m
+++ examples/RRR_robot.m
@@ -4,7 +4,7 @@ l = [0.75; 1; 1]; % link length [m]
 r = [0.1; 0.1; 0.1]; % radius [m]
 m = [0.75; 1; 1]; % mass of each link [kg]
 B = [0; 0; 0]; % buoyancy [N] 
-orient = {'z'; 'x'; 'x'};
+orient = ['z'; 'x'; 'x'];
 Rot0 = eye(3); % rotation matrix
 
 q = sym('q', [n 1], 'real'); % generalized coordinates vector
